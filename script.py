46b2a60de7248629b7d62f3c42c6b47de295d589
Ibrar Munir <ibrarmunir009@gmail.com>
2019-03-04 19:42:54 +0500

    Added the logic to count the score

--- script.py
+++ script.py
@@ -3,3 +3,12 @@ points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 
 
 letter_to_points = {letter: point for letter, point in zip(letters, points)}
 letter_to_points[""] = 0
+
+def score_word(word):
+  point_total = 0
+  for letter in word:
+    point_total += letter_to_points.get(letter,0)
+  return point_total
+
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
